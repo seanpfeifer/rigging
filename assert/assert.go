8bc6e9d7b117386067a03ac2480c39fafc2820b4
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2020-07-05 02:31:01 -0700

    Simplified parts of assert and made it testable

--- assert/assert.go
+++ assert/assert.go
@@ -2,43 +2,50 @@ package assert
 
 import (
 	"fmt"
+	"path/filepath"
 	"reflect"
 	"runtime"
-	"strings"
-	"testing"
 	"time"
 )
 
-// ExpectedActual logs a testing error if the expected and actual values are not equal.
-func ExpectedActual(t *testing.T, expected, actual interface{}, name string) {
-	if expected != actual {
-		errStr := fmt.Sprintf(`[%s] Expected: "%+v".(%s)  Actual: "%+v".(%s)`, name,
-			expected, reflect.TypeOf(expected),
-			actual, reflect.TypeOf(actual))
-
-		logErrorMsg(t, errStr)
-	}
+// The idea here is to accept `*testing.T`, or another impl to be able to test this package itself.
+type Tester interface {
+	Errorf(format string, args ...interface{})
 }
 
-// ExpectedApproxTime logs a testing error if the expected and actual time values are not close.
-func ExpectedApproxTime(t *testing.T, expected, actual time.Time, epsilon time.Duration, name string) {
+// ExpectedActual logs a testing error and returns false if the expected and actual values are not equal.
+// Typically you will not need the return value unless you want to stop testing.
+func ExpectedActual(t Tester, expected, actual interface{}, name string) bool {
+	if expected == actual {
+		return true
+	}
+
+	errStr := fmt.Sprintf(`[%s] Expected: "%+v".(%s)  Actual: "%+v".(%s)`, name,
+		expected, reflect.TypeOf(expected),
+		actual, reflect.TypeOf(actual))
+
+	logErrorMsg(t, errStr)
+	return false
+}
+
+// ExpectedApproxTime logs a testing error and returns false if the expected and actual time values are not close.
+// Specifically, if the difference between the two is LARGER than the given epsilon.
+// Typically you will not need the return value unless you want to stop testing.
+func ExpectedApproxTime(t Tester, expected, actual time.Time, epsilon time.Duration, name string) bool {
 	deltaTime := actual.Sub(expected)
 	if deltaTime > epsilon {
 		errStr := fmt.Sprintf(`[%s] Expected: "%s"  Actual: "%s" Delta: %s Tolerance: %s`, name, expected, actual, deltaTime, epsilon)
 
 		logErrorMsg(t, errStr)
+		return false
 	}
+	return true
 }
 
-func logErrorMsg(t *testing.T, errStr string) {
+func logErrorMsg(t Tester, errStr string) {
 	_, file, line, ok := runtime.Caller(2)
 	if ok {
-		// Truncate file name at last file name separator.
-		if index := strings.LastIndex(file, "/"); index >= 0 {
-			file = file[index+1:]
-		} else if index = strings.LastIndex(file, `\`); index >= 0 {
-			file = file[index+1:]
-		}
+		file = filepath.Base(file)
 	} else {
 		file = "???"
 		line = 1
