8b0feb6c80fe0182f88e67fd555808cbf777ac0b
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2020-08-22 17:43:56 -0700

    Added JSON file unmarshalling helper

---
+++ fileload/json.go
@@ -0,0 +1,16 @@
+package fileload
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// JSON reads a file at the given location and attempts to unmarshal it as JSON into the given value pointed to by v.
+func JSON(filename string, v interface{}) error {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, v)
+}
