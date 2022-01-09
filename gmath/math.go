4fca95962457943547a89c49e0ff90c1a91d6667
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2022-01-08 16:17:19 -0800

    Added gmath for Clamp + Lerp funcs

---
+++ gmath/math.go
@@ -0,0 +1,27 @@
+// Package gmath includes useful math funcs for games, graphics, and GUIs.
+package gmath
+
+import "constraints"
+
+// BasicNumbers is everything that's not a complex number (excludes complex64, complex128)
+type BasicNumbers interface {
+	constraints.Integer | constraints.Float
+}
+
+// Clamp will return the value clamped between min and max, inclusive.
+func Clamp[N BasicNumbers](val, min, max N) N {
+	if val < min {
+		return min
+	} else if val > max {
+		return max
+	}
+	return val
+}
+
+// Lerp linearly interpolates between a and b using t. t is clamped between 0.0 and 1.0.
+// eg, when t is 0.0, this returns a. When t is 1.0 this returns b. When t is 0.5 this returns the midpoint between a and b.
+func Lerp[N BasicNumbers, F constraints.Float](a, b N, t F) N {
+	// If you're using floats, there's effectively no cost to the casting done here with F(b-a).
+	// If you're not, you'd have to cast at some point anyway or otherwise do something clever to get a useful interpolation.
+	return a + N(F(b-a)*Clamp(t, 0, 1))
+}
---
+++ gmath/math_test.go
@@ -0,0 +1,59 @@
+package gmath
+
+import (
+	"testing"
+
+	"github.com/seanpfeifer/rigging/assert"
+)
+
+func TestClamp(t *testing.T) {
+	assert.ExpectedActual(t, 0, Clamp(-1, 0, 100), "clamp to min (int)")
+	assert.ExpectedActual(t, 100, Clamp(101, 0, 100), "clamp to max (int)")
+	assert.ExpectedActual(t, 0.0, Clamp(-1.0, 0.0, 1.0), "clamp to min (float)")
+	assert.ExpectedActual(t, 1.0, Clamp(2.0, 0.0, 1.0), "clamp to max (float)")
+}
+
+// Note: You can run these benchmarks with a command like:
+//    go test -benchtime=20000000000x -benchmem -bench .
+
+var resultF64 float64
+
+func BenchmarkLerpFloat64(b *testing.B) {
+	var res float64
+	for n := 0; n < b.N; n++ {
+		res = Lerp(0.0, 1.0, 0.5)
+	}
+	resultF64 = res
+}
+
+var resultF32 float32
+
+func BenchmarkLerpFloat32(b *testing.B) {
+	var res float32
+	for n := 0; n < b.N; n++ {
+		// Explicitly using float32 here as a convenience so we don't have to cast the params.
+		// In a real-world case you'd just pass your float32 vars / constants.
+		res = Lerp[float32](0.0, 1.0, 0.5)
+	}
+	resultF32 = res
+}
+
+var resultI64 int64
+
+func BenchmarkLerpInt64(b *testing.B) {
+	var res int64
+	for n := 0; n < b.N; n++ {
+		res = Lerp[int64](0, 100, 0.5)
+	}
+	resultI64 = res
+}
+
+var resultI32 int32
+
+func BenchmarkLerpInt32(b *testing.B) {
+	var res int32
+	for n := 0; n < b.N; n++ {
+		res = Lerp[int32](0, 100, 0.5)
+	}
+	resultI32 = res
+}
